0002503f193bba22c007b1e6d9af2fca2c55838a
jim-low <robil12380@gmail.com>
2024-05-22 18:41:06 +0800

    feat: add super admin CRUD file

---
+++ server/api/super-admin.js
@@ -0,0 +1,14 @@
+function createUser(req, res) { }
+
+function getUser(req, res) { }
+
+function updateUser(req, res) { }
+
+function deleteUser(req, res) { }
+
+export {
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+}
