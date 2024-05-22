c92d2776d604baeb5b26d8324a9c132425746361
jim-low <robil12380@gmail.com>
2024-05-22 18:51:08 +0800

    feat: add create user endpoint with functionality

--- server/api/super-admin.js
+++ server/api/super-admin.js
@@ -1,4 +1,23 @@
-function createUser(req, res) { }
+import { query } from "../db.js";
+
+async function createUser(req, res) {
+  const { username, password, email } = req.body;
+  if (username.length === 0 || password.length === 0 || email.length === 0) {
+    return res.status(418).send({ message: "Required input missing!" })
+  }
+
+  try {
+    await query(`
+INSERT INTO test_users(username, password, email)
+VALUES ('${username}', '${password}', '${email}');
+`)
+    return res.status(200).send({ message: "User added successfully" })
+  }
+  catch(err) {
+    console.error(err)
+    return res.status(418).send({ message: "[ERROR] Trouble adding new user" })
+  }
+}
 
 function getUser(req, res) { }
 
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import login from './api/login/login.js'
 import { whatisthis } from './api/whatisthis.js'
+import { createUser } from './api/super-admin.js'
 dotenv.config()
 
 const app = express()
@@ -14,6 +15,8 @@ app.use('/login', loginRouter)
 
 app.get('/whatisthis', whatisthis)
 app.post('/login/:loginType', login)
+
+app.post('/super-admin/create-user', createUser)
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server started at port: ${process.env.SERVER_PORT}`)
