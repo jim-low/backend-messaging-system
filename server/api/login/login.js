0d7dd2d0a908421676a620de3ff6cdb6109484db
jim-low <robil12380@gmail.com>
2024-05-23 14:05:14 +0800

    feat: add JWT tokens after authenticating user credentials

--- server/api/login/login.js
+++ server/api/login/login.js
@@ -1,4 +1,8 @@
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
 import { query } from '../../db.js'
+import dotenv from 'dotenv'
+dotenv.config()
 
 async function login(req, res) {
   const { loginType } = req.params;
@@ -12,19 +16,31 @@ async function login(req, res) {
 
   try {
     const result = await query(`
-SELECT u.user_id, username, email
+SELECT u.user_id, username, email, password
   FROM ${table}_users n
   JOIN users u
   ON n.user_id = u.user_id
-  AND u.email = '${email}'
-  AND u.password = '${password}';
+  AND u.email = '${email}';
 `)
 
     if (result.rows.length <= 0) {
       return res.status(401).send({ message: "Unable to find user" })
     }
 
-    res.status(200).send({ data: result.rows })
+    const isSamePassword = await bcrypt.compare(password, result.rows[0].password)
+    if (!isSamePassword) {
+      return res.status(418).send({ message: "Incorrect credentials" })
+    }
+
+    // user credentials are verified
+    const { userName, userEmail } = result.rows[0]
+    const user = {
+      username: userName,
+      email: userEmail
+    }
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN)
+
+    res.status(200).send({ accessToken })
   }
   catch(err) {
     console.log(err)
