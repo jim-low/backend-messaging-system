5499f4cde545c7c3a024857480f9f234d552e7fa
jim-low <robil12380@gmail.com>
2024-05-26 01:01:57 +0800

    fix: add cors policy to allow local development origin

--- server/socket-io/socket-io-server.js
+++ server/socket-io/socket-io-server.js
@@ -3,7 +3,11 @@ const { query } = require('../db')
 
 require('dotenv').config()
 
-const io = require('socket.io')(process.env.WEBSOCKET_PORT)
+const io = require('socket.io')(process.env.WEBSOCKET_PORT, {
+  cors: {
+    origin: ['http://127.0.0.1:8080']
+  }
+})
 io.on('connection', socket => {
   console.log("New user connected")
   socket.on('send-message', (token, toUser, message) => {
