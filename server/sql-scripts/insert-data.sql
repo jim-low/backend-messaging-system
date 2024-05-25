d0a1fc379f0a35bbece0dd05e430ea90eb00863d
jim-low <robil12380@gmail.com>
2024-05-25 16:58:00 +0800

    fix: add nested queries to add users into respective tables

--- server/sql-scripts/insert-data.sql
+++ server/sql-scripts/insert-data.sql
@@ -4,13 +4,13 @@ INSERT INTO users(username, email, password) VALUES ('Mike Oxmaul', 'mike@gmail.
 INSERT INTO users(username, email, password) VALUES ('Joe Mudder', 'joe@gmail.com', 'password4');
 INSERT INTO users(username, email, password) VALUES ('Saw Con', 'saw@gmail.com', 'password5');
 
-INSERT INTO super_admin_users(user_id) VALUES (1); -- Jim
+INSERT INTO super_admin_users(user_id) VALUES ((SELECT user_id FROM users WHERE username = 'Jim')); -- Jim
 
-INSERT INTO admin_users(user_id) VALUES (2); -- Ben Dover
-INSERT INTO admin_users(user_id) VALUES (3); -- Mike Oxmaul
+INSERT INTO admin_users(user_id) VALUES ((SELECT user_id FROM users WHERE username = 'Ben Dover')); -- Ben Dover
+INSERT INTO admin_users(user_id) VALUES ((SELECT user_id FROM users WHERE username = 'Mike Oxmaul')); -- Mike Oxmaul
 
-INSERT INTO normal_users(user_id) VALUES (4); -- Joe Mudder
-INSERT INTO normal_users(user_id) VALUES (5); -- Saw Con
+INSERT INTO normal_users(user_id) VALUES ((SELECT user_id FROM users WHERE username = 'Joe Mudder')); -- Joe Mudder
+INSERT INTO normal_users(user_id) VALUES ((SELECT user_id FROM users WHERE username = 'Saw Con')); -- Saw Con
 
 -- messages data
 INSERT INTO messages(from_user_id, to_user_id, message) VALUES (1, 2, 'Hello Ben Dover');
