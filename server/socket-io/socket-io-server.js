25564cf9af49a3a01b1abc4ce1119d7fc14f1cfb
jim-low <robil12380@gmail.com>
2024-05-25 16:58:27 +0800

    chore: remove console log

--- server/socket-io/socket-io-server.js
+++ server/socket-io/socket-io-server.js
@@ -2,10 +2,7 @@ require('dotenv').config()
 
 const io = require('socket.io')(8000)
 io.on('connection', socket => {
-  console.log("New user has connected")
-
   socket.on('send-message', (fromUser, toUser, message) => {
-    console.log(fromUser, toUser, message)
     io.emit('receive-message', message)
   })
 })
