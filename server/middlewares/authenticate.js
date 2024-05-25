4fd7ac8b9e2ab3f1c7e9847e02c05140638a7438
jim-low <robil12380@gmail.com>
2024-05-25 21:00:44 +0800

    refactor: change function name to authenticate, export function

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken')
 
-function authenticateToken(req, res, next) {
+function authenticate(req, res, next) {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
 
@@ -13,6 +13,4 @@ function authenticateToken(req, res, next) {
   })
 }
 
-export {
-  authenticateToken
-}
+exports.authenticate = authenticate
