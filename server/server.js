8fe9683bbe90d6069d716614bdb11494425119d3
jim-low <robil12380@gmail.com>
2024-05-26 14:06:26 +0800

    refactor: rename getUserList to getUsersList

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const { login } = require('./api/login/login.js')
 const { whatisthis } = require('./api/whatisthis.js')
-const { createUser, deleteUser, getUser, getUserList, updateUser } = require('./api/super-admin.js')
+const { createUser, deleteUser, getUser, getUsersList, updateUser } = require('./api/super-admin.js')
 const { getMessages } = require('./api/message/get-messages.js')
 const { authenticate } = require('./middlewares/authenticate.js')
 const { io } = require('./socket-io/socketio-server.js') // enable WebSocket server
@@ -18,7 +18,7 @@ app.post('/login/:loginType', login)
 
 app.post('/super-admin/create-user/:userType', createUser)
 app.get('/super-admin/get-user/:userId', getUser)
-app.get('/super-admin/get-users', authenticate, getUserList)
+app.get('/super-admin/get-users', authenticate, getUsersList)
 app.post('/super-admin/update-user', updateUser)
 app.post('/super-admin/delete-user', deleteUser)
 
