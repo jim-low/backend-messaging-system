ca565122b40a6489f4382a4e677c44335b6ad0e1
jim-low <robil12380@gmail.com>
2024-05-28 13:27:38 +0800

    fix: send error message to client

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -2,10 +2,14 @@ const jwt = require('jsonwebtoken')
 
 function authenticate(req, res, next) {
   const token = req.headers['authorization']
-  if (token == null) return res.sendStatus(401)
+  if (token == null) {
+    return res.status(401).send({ error: "Unauthorized user" })
+  }
 
   jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
-    if (err) return res.sendStatus(403)
+    if (err) {
+      return res.status(403).send({ error: "Error authenticating user" })
+    }
     req.user = user
     next()
   })
